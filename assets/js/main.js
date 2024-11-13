8feacd3c28ebc6ebe91ae0404059324f2ec8027d
hmaminulislam <mdaminulislamsahin14@gmail.com>
2024-11-13 09:15:16 +0600

    hero section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,19 @@
 
 
 
+	// marquee start 
 
+	$('.rb-marquee-1').marquee({
+		speed: 80,
+		gap: 30,
+		delayBeforeStart: 0,
+		direction: 'left',
+		duplicated: true,
+		pauseOnHover: true,
+		startVisible:true,
+	})
+
+	// marquee end
 
 
 })
