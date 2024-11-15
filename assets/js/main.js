02bd305f9bbdf1ac509996faa7db5342df4c5c97
hmaminulislam <mdaminulislamsahin14@gmail.com>
2024-11-15 08:14:57 +0600

    services, work , testimonial and footer section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,82 @@
 
 	"use strict";
 
+
+	// work-1 slider start
+
+	let rbWork1Slider = new Swiper('.rb-work-1-slider', {
+		loop: true,
+		speed: 1000,
+		spaceBetween: 0,
+		autoplay: {
+			delay: 3000,
+		},
+		navigation: {
+			nextEl: ".rb-work-1-next",
+			prevEl: ".rb-work-1-prev",
+		},
+
+		breakpoints: {
+			0: {
+				slidesPerView: 1,
+				spaceBetween: 30,
+			},
+			576: {
+				slidesPerView: 1,
+				spaceBetween: 30,
+			},
+			768: {
+				slidesPerView: 1,
+				spaceBetween: 30,
+			},
+			992: {
+				slidesPerView: 2,
+				spaceBetween: 40,
+			},
+			1200: {
+				slidesPerView: 3,
+			},
+			1400: {
+				slidesPerView: 3,
+			},
+			1600: {
+				slidesPerView: 3,
+			},
+			1800: {
+				slidesPerView: 3,
+			},
+		}
+	});
+
+
+	// work-1 slider end
+
+
+
+
+	// work-1 slider start
+
+	let rbTestimonial1Slider = new Swiper('.rb-testimonial-1-slider', {
+		loop: true,
+		speed: 1000,
+		spaceBetween: 80,
+		// autoplay: {
+		// 	delay: 4000,
+		// 	disableOnInteraction: false
+		// },
+
+		navigation: {
+			nextEl: ".rb-testimonial-1-next",
+			prevEl: ".rb-testimonial-1-prev",
+		},
+	});
+
+
+	// work-1 slider end
+
+
+
+
 	// background img activation start
 
 	$("[data-background]").each(function(){
@@ -27,8 +103,6 @@
 	}); 
 
 	// counter end
-
-
 
 
 	// marquee start 
